ea621a8537081b73de5ebd4ccdefc3c0586576b3
ssshah2131 <33388945+ssshah2131@users.noreply.github.com>
2024-03-22 12:03:40 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>POC by Shail Shah</title>
+</head>
+<body>
+    <h1>Hello Team! This is Subdomain Takeover POC created by Shail Shah </h1>
+    <?php
+        
+    ?>
+</body>
+</html>
